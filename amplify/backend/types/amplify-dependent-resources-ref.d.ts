383606a933fcdcbadd32950a7240ad674a49c037
sanjanaravikumar-az <sanjana.ravikumar.az@gmail.com>
2026-04-14 16:55:51 -0400

    feat: add storage

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -5,5 +5,22 @@ export type AmplifyDependentResourcesAttributes = {
       "GraphQLAPIIdOutput": "string",
       "GraphQLAPIKeyOutput": "string"
     }
+  },
+  "auth": {
+    "financetracker2482cfa19": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "storage": {
+    "s310def45a": {
+      "BucketName": "string",
+      "Region": "string"
+    }
   }
 }
